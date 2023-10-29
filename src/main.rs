cff720d16aa2a306b7d57db1caf92b3277d22997
brandon engstrom <bengstrom32@gmail.com>
2023-10-29 11:22:41 -0400

    another commit

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@ extern crate tiny_keccak;
 use tiny_keccak::Keccak;
 use std::io;
 
+// function takes a string and converts it to a keccak hash
+// using the new_sha3_256 function. 
+
+// Keccak256 is a cryptographic hash function that takes an 
+// input of an arbitrary length and produces a fixed-length output 
+// of 256 bits. It is the function used to compute the hashes 
+// of Ethereum addresses, transaction IDs, and other important values 
+// in the Ethereum ecosystem.
+
+
 fn main() {
     println!("Enter the string to hash:");
     let mut input = String::new();
