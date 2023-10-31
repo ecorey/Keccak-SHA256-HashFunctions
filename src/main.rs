746d2d356f86f0393559baf1e5eea6f7bf7c2d2a
brandon engstrom <bengstrom32@gmail.com>
2023-10-31 12:20:29 -0400

    main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate tiny_keccak;
+extern crate sha2;
+
 use tiny_keccak::Keccak;
+use sha2::{Sha256, Digest};
 use std::io;
 
 // function takes a string and converts it to a keccak hash
@@ -10,8 +13,6 @@ use std::io;
 // of 256 bits. The Keccak256 hash function is the function used to compute 
 // the hashes of Ethereum addresses, transaction IDs, and other values.
 
-
-
 fn main() {
     println!("Enter the string to hash:");
 
@@ -19,6 +20,7 @@ fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read line");
     
+    // KECCAK-256 HASHING
     // create var sha3 to the hashed value
     let mut sha3 = Keccak::new_sha3_256();
 
@@ -39,4 +41,18 @@ fn main() {
 
     // prints the hashed value
     println!("Keccak hash: {:?}", res);
+    
+    // SHA-256 HASHING
+    // create a Sha256 object
+    let mut sha256 = Sha256::new();
+
+    // update the hash object with the byte data.
+    sha256.update(data);
+
+    // finalize the hash computation and store the 
+    // result in a new array.
+    let sha256_res = sha256.finalize();
+
+    // prints the hashed value
+    println!("SHA-256 hash: {:?}", sha256_res);
 }
